9f88f966105d57d6bd0d6f74de71f2cc61edef6c
zxy <zhangxycg@126.com>
2019-11-13 20:33:49 +0800

    服务平台项目

--- src/main/java/com/zjcds/entity/DataResourceInfo.java
+++
@@ -1,9 +0,0 @@
-package com.zjcds.entity;
-
-public class DataResourceInfo {
-	private int id;
-	private String sname;
-	private String sex;
-	private int score;
-
-}
--- src/main/java/com/zjcds/entity/ServiceInfo.java
+++
@@ -1,9 +0,0 @@
-package com.zjcds.entity;
-
-public class ServiceInfo {
-	private int id;
-	private String sname;
-	private String sex;
-	private int score;
-
-}
